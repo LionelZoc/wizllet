30b795b56598a7e6e59fdfcc08333672a6deb981
Mylene Orliaguet <mylene@wisembly.com>
2022-09-15 15:04:07 +0200

    add create form

--- web/src/event/Create.js
+++ web/src/event/Create.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { css } from '@emotion/react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+import { useGetEvents, useSetEvent, useDeleteEvent } from "../db/events";
+
 const Create = ({ onClose }) => {
+  const setEvent = useSetEvent();
   const [values, setValues] = useState({
     title: "",
     description: "",
@@ -15,11 +19,17 @@ const Create = ({ onClose }) => {
     setValues({...values, [field]: value});
   }
 
+  const onSubmit = () => {
+    setEvent(values)
+      .then(onClose);
+  }
+
   return (
-    <Box>
+    <Box display="flex" flexDirection="column">
       <Button onClick={onClose}>Return</Button>
-      <TextField value={values.title} onChange={(event) => handleChange("title", event.target.value)} />
-      <TextField value={values.description} onChange={(event) => handleChange("description", event.target.value)} />
+      <TextField label="Title" margin="dense" value={values.title} onChange={(event) => handleChange("title", event.target.value)} />
+      <TextField label="Description" margin="dense" value={values.description} onChange={(event) => handleChange("description", event.target.value)} />
+      <Button variant="contained" onClick={() => onSubmit(values)}>Submit</Button>
     </Box>
   );
 }
