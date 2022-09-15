b51c59aa2b2c0c641bde147864e7c40af4aa1376
Julien Grelet <juliengrelet@MacBook-Pro.local>
2022-09-15 10:53:11 +0200

    feature(firebase): Adding firebase in project

---
+++ web/src/db/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCg9kUOeZFYTbPkyY3pi2-tznnfJS3SYvo",
+  authDomain: "wizllet-83661.firebaseapp.com",
+  projectId: "wizllet-83661",
+  storageBucket: "wizllet-83661.appspot.com",
+  messagingSenderId: "686717924613",
+  appId: "1:686717924613:web:16495674d1c2c64460736c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
