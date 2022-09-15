7c911b65bfd64ea73e52e3f70ddf214fdf61aaed
Julien Grelet <juliengrelet@MacBook-Pro.local>
2022-09-15 14:46:51 +0200

    feature(crud): Create crud firebase

--- web/src/db/events.js
+++ web/src/db/events.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "./index";
-import { getDocs, collection, onSnapshot } from "firebase/firestore"; 
+import { addDoc, doc, updateDoc, collection, onSnapshot, deleteDoc } from "firebase/firestore"; 
 
-const useEventsManager = () => {
+const useGetEvents = () => {
     const [events, setEvents] = useState([]);
 
     const getEvents = () => {
         onSnapshot(collection(db, 'events'), (querySnapshot) => {
-            setEvents(querySnapshot.docs.map(event => event.data()));
-        });
+            setEvents(querySnapshot.docs.map(event => {
+                return {...event.data(), id: event.id }
+            }));
+        })
     }
 
     useEffect(() => {
@@ -18,9 +20,38 @@ const useEventsManager = () => {
     return events;
 }
 
-const addEvent = () => {}
+const useSetEvent = () => {
+    const add = async (data = {}) => {
+        const obj = {
+            title: data?.title || "",
+            description: data?.description || "",
+            logo: data?.logo || "",
+            qrcode: data?.qrcode || ""
+        }
+        try {
+            await addDoc(collection(db, 'events'), obj)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return add;
+}
+ 
+const useDeleteEvent = () => {
+    const remove = async (id) => {
+        const docRef = doc(db, "events", id);
+        try {
+            await deleteDoc(docRef, {})
+        } catch(e) {
+            console.error(e);
+        }
+    }
+    return remove;
+}
 
 export {
-    useEventsManager,
-    addEvent
+    useGetEvents,
+    useSetEvent,
+    useDeleteEvent
 }
